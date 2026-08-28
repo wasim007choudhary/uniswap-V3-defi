0232f31b40210ed0ffca0515f82f7b5233fdf1d6
wasim007choudhary <lesnarwasimff5@gmail.com>
2026-08-28 23:29:19 +0530

    syntax error fxed

--- contracts/coreUV3/library/SwapMath.sol
+++ contracts/coreUV3/library/SwapMath.sol
@@ -6,14 +6,11 @@ import {MyCustomFullMath} from "contracts/coreUV3/library/MyCustomFullMath.sol";
 import {SqrtPriceMath} from "contracts/coreUV3/library/SqrtPriceMath.sol";
 
 library SwapMath {
-
     function computeSwapStep(
         uint160 currentSqrtRatioPriceX96,
         uint160 targetSqrtRatioPriceX96,
         uint128 liquidity,
         int256 amountRemaining,
         uint24 feepips
-    ) internal pure returns () {
-        
-    }
+    ) internal pure returns(){}
 }
