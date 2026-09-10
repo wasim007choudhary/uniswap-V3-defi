18fa17778c09d547cad650b4f22dbf23a9302371
wasim007choudhary <lesnarwasimff5@gmail.com>
2026-09-11 03:30:57 +0530

    TickMath.sol 2.ActualCodeStarting.md notes updated

--- contracts/coreUV3/library/TickMath.sol
+++ contracts/coreUV3/library/TickMath.sol
@@ -9,5 +9,5 @@ library TickMath {
     uint160 internal constant MIN_SQRT_RATIO = 4295128739; // the minimum v3 suported sqrtPrice here supported..or basically is the sqrtPrice of MIN_VALID_TICK
     uint160 internal constant MAX_SQRT_RATIO = 1461446703485210103287273052203988822378723970342; // same thing but for the Max part.GGs easy!
 
-    function getSqrtRatioAtTick(int24 tick) internal pure returns (uint160 sqrtPriceX96OfTheTick) {}
+    function getSqrtPriceRatioAtTick(int24 tick) internal pure returns (uint160 sqrtPriceX96OfTheTick) {}
 }
