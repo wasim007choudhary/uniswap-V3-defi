c6619c018d2d06b33729efd8f2a7dee8863217a6
wasim007choudhary <lesnarwasimff5@gmail.com>
2026-09-11 02:51:05 +0530

    TickMath 2.notes/CoreLibFunctions/TickMath/2.ActualCodeStarting.md   added

--- contracts/coreUV3/library/TickMath.sol
+++ contracts/coreUV3/library/TickMath.sol
@@ -3,6 +3,6 @@
 pragma solidity ^0.8.20;
 
 library TickMath {
-    int24 internal constant MIN_VALID_TICK = -887272;
-    int24 internal constant MAX_VALID_TICK = -MIN_VALID_TICK;
+    int24 internal constant MIN_VALID_TICK = -887272; // min tick limit or uniswap limit no more tick beyond that and also the last initlizable tick which can be converted or priceroot can be taken from
+    int24 internal constant MAX_VALID_TICK = -MIN_VALID_TICK; //same shit but it is for maximumn limit
 }
